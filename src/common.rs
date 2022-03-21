62dbced37559f1086cd01c8c4abdcb5c364a48e1
William D. Jones <thor0505@comcast.net>
2022-03-20 23:47:48 -0400

    cargo fmt. Bump version.

--- src/common.rs
+++ src/common.rs
@@ -1,4 +1,4 @@
-use downcast_rs::{Downcast, impl_downcast};
+use downcast_rs::{impl_downcast, Downcast};
 pub use embedded_hal as hal_traits;
 pub use linux_embedded_hal as hal;
 pub use pico_args::Arguments;
@@ -13,6 +13,12 @@ pub trait Cmd {
 pub trait CmdParameters: Downcast {}
 impl_downcast!(CmdParameters);
 
-pub fn downcast_params<T>(params: &Box<dyn CmdParameters>) -> Result<&T> where T: CmdParameters {
-    Ok(params.downcast_ref::<T>().ok_or(eyre!(format!("could not extract parameters, expected type {}", std::any::type_name::<T>())))?)
+pub fn downcast_params<T>(params: &Box<dyn CmdParameters>) -> Result<&T>
+where
+    T: CmdParameters,
+{
+    Ok(params.downcast_ref::<T>().ok_or(eyre!(format!(
+        "could not extract parameters, expected type {}",
+        std::any::type_name::<T>()
+    )))?)
 }
